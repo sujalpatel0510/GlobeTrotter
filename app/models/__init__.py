fb93dbe951f15cd38ab63971d147b47bdc181679
sujalpatel0510 <sujalpatel0510@gmail.com>
2026-08-22 13:37:48 +0530

    commit

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -10,3 +10,10 @@ migrate = Migrate()
 login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Please log in to access this page.'
 login_manager.login_message_category = 'info'
+
+# Explicitly import all models for metadata discovery and table creation
+from app.models.user import User
+from app.models.trip import Trip
+from app.models.itinerary import ItinerarySection, ItineraryItem
+from app.models.destination import Destination, Activity
+from app.models.community import CommunityPost
